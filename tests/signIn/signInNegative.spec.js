8964a547ef19f8cd4a1c80f90279c84c9b0d4451
Inna Ianko <inna.i@mate.academy>
2025-05-26 14:16:12 +0300

    Minor style fix

--- tests/signIn/signInNegative.spec.js
+++ tests/signIn/signInNegative.spec.js
@@ -1,31 +1,32 @@
 import { test, expect } from '@playwright/test';
 
 test.describe('Sign in negative tests', () => {
-
-  test.beforeEach( async ({ page }) => {
+  test.beforeEach(async ({ page }) => {
     await page.goto('https://conduit.mate.academy/user/login');
   });
 
   test('Assert error message for empty password', async ({ page }) => {
-  await page.getByPlaceholder('Email').fill('test@gmail.com');
-  await page.getByRole('button', { name: 'Sign in' }).click();
-
-  await expect(page.getByRole('list').nth(1)).toContainText(`password:can\'t be blank`);
+    await page.getByPlaceholder('Email').fill('test@gmail.com');
+    await page.getByRole('button', { name: 'Sign in' }).click();
+    await expect(page.getByRole('list').nth(1)).toContainText(
+      `password:can't be blank`,
+    );
   });
 
   test('Assert error message for empty email', async ({ page }) => {
     await page.getByPlaceholder('Password').fill('newpass123!');
     await page.getByRole('button', { name: 'Sign in' }).click();
-  
-    await expect(page.getByRole('list').nth(1)).toContainText(`email:can't be blank`);
+    await expect(page.getByRole('list').nth(1)).toContainText(
+      `email:can't be blank`,
+    );
   });
 
   test('Assert error message for wrong password', async ({ page }) => {
     await page.getByPlaceholder('Email').fill('test@gmail.com');
     await page.getByPlaceholder('Password').fill('1');
     await page.getByRole('button', { name: 'Sign in' }).click();
-  
-    await expect(page.getByRole('list').nth(1)).toContainText(`email or password:is invalid`);
+    await expect(page.getByRole('list').nth(1)).toContainText(
+      `email or password:is invalid`,
+    );
   });
-
 });
--- tests/signIn/signInPositive.spec.js
+++ tests/signIn/signInPositive.spec.js
@@ -1,12 +1,11 @@
 import { test, expect } from '@playwright/test';
 
 test('Successful `Sign in` flow test', async ({ page }) => {
-
- await page.goto('https://conduit.mate.academy/user/login');
- 
- await page.getByPlaceholder('Email'). fill('test_new_user@gmail.com');
- await page.getByPlaceholder('Password'). fill('newpass123!');
- await page.getByRole('button', { name: 'Sign in' }).click();
-
- await expect(page.getByText('Your Feed')).toBeVisible();
+  const email = ''; // TODO add email
+  const password = ''; // TODO add password
+  await page.goto('https://conduit.mate.academy/user/login');
+  await page.getByPlaceholder('Email').fill(email);
+  await page.getByPlaceholder('Password').fill(password);
+  await page.getByRole('button', { name: 'Sign in' }).click();
+  await expect(page.getByText('Your Feed')).toBeVisible();
 });
